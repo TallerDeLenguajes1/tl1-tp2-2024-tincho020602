6a07c05b387181204dafaf692e46692e8c4829f6
tincho020602 <ezequiel.ibarra020602@gmail.com>
2024-04-12 16:39:07 -0300

    agrego tp2_1_2.c y respuestas.txt

---
+++ tp2_1_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 20
+int main(){ 
+int i;
+srand(time(NULL));
+double vt[N];
+for(i = 0;i<N; i++)
+{
+*(vt+1)=1+rand()%100;
+printf("%2.f ", *(vt+1));
+}
+return 0;
+}
